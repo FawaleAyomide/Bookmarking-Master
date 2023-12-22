5b719cf2444115e6fd87b983296a9d768b420bcb
Fawale Ayomide Oladele <109774878+FawaleAyomide@users.noreply.github.com>
2023-12-22 12:54:45 +0100

    Update Home.js

--- src/Section/Home.js
+++ src/Section/Home.js
@@ -268,7 +268,7 @@ const Home = () => {
               <div className="img"></div>
             </Link>
             </div>
-           <div class="attribution" > Coded by <a href="https://my-portfolio-swart-xi.vercel.app/">Zcodes</a>..</div>
+           <div class="attribution" > Coded by <a href="https://portfolio-ten-chi-17.vercel.app/">Zcodes</a>..</div>
           </div>
            </div>
       </footer>
